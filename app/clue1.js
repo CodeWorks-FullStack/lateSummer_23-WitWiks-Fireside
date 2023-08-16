3953401297728bbf7c9f6b4601716172d06d32a9
Miles-Collins <miles.collins@ymail.com>
2023-08-16 09:37:41 -0600

    🚀🚀🚀🚀🚀🚀

---
+++ app/clue1.js
@@ -0,0 +1,31 @@
+// We have intercepted a letter sent to one of her accomplices
+
+// A message sent to her associate requires we decode it using a letter within a certain position of a string.
+// Ex: ('Cryptology', 3) should return 'p'
+function letterAtPosition(str, position) {
+  // TODO YOUR CODE HERE
+ 
+}
+
+// It has also been discovered some of the words have been separated and need to be joined back together, sometimes with an extra separator, if one is not provided assume that it will be an empty space
+
+// Write a function that will join two strings with an optional separating character
+function concat(str1, str2, separator = " ") {
+  // TODO YOUR CODE HERE
+
+}
+
+// Our cryptologists have discovered that some of the letters that were separated out should also be capitalized.
+
+// Write a function that will capitalize the letter at a given position and return it
+function capitalizer(str, position) {
+  // TODO YOUR CODE HERE
+ 
+}
+
+// There are a few key words we need to look for to find if they are in the message
+// write a function that will take in two strings and identify if the first string 'includes' the second
+function messageContainsPhrase(message, phrase) {
+  // TODO YOUR CODE HERE
+
+}
